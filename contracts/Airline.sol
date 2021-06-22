43cb4c221c9a14ee14386a664b0fe97c83fb25a0
MARIANO SELVAGGI <mariano_selvaggi@yahoo.com.ar>
2021-06-22 12:45:58 -0300

    first commit

---
+++ contracts/Airline.sol
@@ -0,0 +1,68 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Airline {
+    address public owner;
+    
+    struct Customer {
+        uint loyaltyPoints;
+        uint totalFlights;
+    }
+
+    struct Flight {
+        string name;
+        uint price;
+    }
+    uint etherPerPoint = 0.5 ether;
+    Flight[] public flights;
+
+    constructor() public {
+        owner = msg.sender;
+        flights.push(Flight('Tokio',4 ether));
+        flights.push(Flight('Germany',3 ether));
+        flights.push(Flight('Madrid',3 ether));
+    }
+
+    mapping(address => Customer) public customers;
+    mapping(address => Flight[]) public customerFlights;
+    mapping(address => uint) public customerTotalFlights;
+
+    event FlightPurchased(address indexed customer, uint price);
+
+    function buyFlight(uint flightIndex) public payable {
+        Flight storage flight = flights[flightIndex];
+        require(msg.value == flight.price);
+
+        Customer storage customer = customers[msg.sender];
+        customer.loyaltyPoints += 5;
+        customer.totalFlights += 1;
+        customerFlights[msg.sender].push(flight);
+        customerTotalFlights[msg.sender] ++;
+
+        emit FlightPurchased(msg.sender, flight.price);
+    }
+
+    function totalFlights() public view returns(uint) {
+        return flights.length;
+    }
+
+    function redeemLoyaltyPoints() public {
+        Customer storage customer = customers[msg.sender];
+        uint etherToRefund = etherPerPoint * customer.loyaltyPoints;
+        msg.sender.transfer(etherToRefund);
+        customer.loyaltyPoints = 0;
+    }
+
+    function getRefundableEther() public view returns(uint) {
+        return etherPerPoint * customers[msg.sender].loyaltyPoints;
+    }
+
+    function getAirlineBalance() public isOwner view returns(uint) {
+        address airlineAddress = address(this);
+        return airlineAddress.balance;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
